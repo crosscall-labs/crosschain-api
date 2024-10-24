3d8949fa2149a22351cc404d8339cb0e5389e01e
FudgyDRS <yetimonsters@gmail.com>
2024-10-23 23:54:39 -0700

    added hasint and free cors

--- pkg/utils/helpers.go
+++ pkg/utils/helpers.go
@@ -170,3 +170,31 @@ func Str2Bytes(hexStr string) ([]byte, error) {
 
 	return bytes, nil
 }
+
+func HasInt(inputArray []int, input int) error {
+	for _, value := range inputArray {
+		if value == input {
+			return nil
+		}
+	}
+	return fmt.Errorf("int not found in input array")
+}
+
+func EnableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Set CORS headers
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		// Handle preflight requests
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		fmt.Printf("Method: %s, URL: %s", r.Method, r.URL)
+
+		next.ServeHTTP(w, r)
+	})
+}
